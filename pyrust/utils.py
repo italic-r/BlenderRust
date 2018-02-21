6deb29446f5e47046e5aa08e750abe1fb63246e2
italic <italic.rendezvous@gmail.com>
2018-02-20 22:09:53 -0800

    utils: use explicit pathing to load library, since ctypes.cdll.LoadLibrary did not search in its own directory.

--- pyrust/utils.py
+++ pyrust/utils.py
@@ -1,12 +1,15 @@
 #!/bin/env python3
 
+import pathlib
 import sys
 import ctypes
 
 
 def get_lib(libname):
+    """Load native library from module's directory root."""
+    pack_root = pathlib.Path(__file__).parent.resolve()
     prefix = {"win32": ""}.get(sys.platform, "lib")
     extension = {"darwin": ".dylib", "win32": ".dll"}.get(sys.platform, ".so")
-    lib = ctypes.cdll.LoadLibrary(prefix + libname + extension)
+    lib = ctypes.cdll.LoadLibrary(pack_root / (prefix + libname + extension))
 
     return lib
